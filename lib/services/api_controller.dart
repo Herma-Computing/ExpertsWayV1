0ac8d15b2b1443c0d90663639248f5705c4ffda5
Esubalew <esubalew.a2009@gmail.com>
2023-05-03 11:16:15 +0300

    token added on get courses

--- lib/services/api_controller.dart
+++ lib/services/api_controller.dart
@@ -2,24 +2,27 @@ import 'package:dio/dio.dart';
 import 'package:learncoding/models/course.dart';
 import 'package:learncoding/models/lesson.dart';
 import 'package:learncoding/utils/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiProvider {
   Future<Course> retrieveCourses() async {
+    String? lastUpdate;
+    
+    
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rhc2hlbmNvbi5jb20vdGVzdCIsImlhdCI6MTY4MzEwMTczMSwibmJmIjoxNjgzMTAxNzMxLCJleHAiOjE3NDYxNzM3MzEsImRhdGEiOnsidXNlciI6eyJpZCI6IjEyIn19fQ.TGqBDOF3M5BqlUZiiciyQwFm1Gj13CKfZh6bjILQpkk");
     var dio = Dio();
-    Response response = await dio.get(
-      AppUrl.courseUrl,
-      queryParameters: {'last_updated': '2020-10-14 06:48:28'},
-      options: Options(
-        headers: {
-          'username': AppUrl.username,
-          'password': AppUrl.password,
-          'login_with': AppUrl.loginWith,
-        },
-      ),
-    );
+    dio.options.headers['content-Type'] = 'application/json';
+    dio.options.headers["Authorization"] = "Bearer ${prefs.getString("token")}";
+    Response response = await dio.get(AppUrl.courseUrl, queryParameters: {'last_updated': lastUpdate != null ? lastUpdate : '2020-10-14 06:48:28'});
     if (response.statusCode == 200) {
       final responseBody = response.data;
       final Course course = courseFromJson(responseBody);
+      if (course.courses.isNotEmpty) {
+        SharedPreferences pref = await SharedPreferences.getInstance();
+        pref.setString('new_lastupdate_date', course.courses.last.lastUpdated.toString());
+      }
       return course;
     } else {
       throw Exception('Failed to load course');
